59c38f94e6e339a9cc1040ff65a7e2c82e723a2f
KarlH3644 <43153810+karl3644@users.noreply.github.com>
2023-04-03 12:14:30 +0100

    basic weather app working using fetch and handling either success or fail response

--- Intermediate - Weather App/WeatherApp.js
+++ Intermediate - Weather App/WeatherApp.js
@@ -1,9 +1,9 @@
 import { api as key } from "./Util.js";
 
-function logger(){
-    console.log(key);
-    console.log('logger');
-}
+// function logger(){
+//     console.log(key);
+//     console.log('logger');
+// }
 
 function inputtedData() {
     const location = document.getElementById("location").value
@@ -14,71 +14,87 @@ function inputtedData() {
 
 function showFailure(failMessage) {
     const para = document.getElementById("failure")
-    console.log('para', para);
     para.textContent = failMessage;
     para.hidden = false;
 }
 
-function setResults(result) {
-    const para = document.getElementById("results")
-    
-    para.textContent = results.current.temp_c;
-    para.textContent = results.current.temp_f;
-    para.textContent = results.current.humidity;
-    para.textContent = results.current.wind_mph;
-    para.textContent = results.current.wind_degree;
-    para.textContent = results.current.wind_dir;
-    para.textContent = results.current.feelslike_c;
-    para.textContent = results.current.feelslike_f;
-    para.textContent = results.current.pressure_mb;
-    para.textContent = results.current.precip_mm;
-    para.textContent = results.current.uv;
+function hideFailure() {
+    const para = document.getElementById("failure")
+    para.hidden = true;
 }
 
-// {
-//     "last_updated_epoch": 1680192900,
-//     "last_updated": "2023-03-30 23:15",
-//     "temp_c": -6.2,
-//     "temp_f": 20.8,
-//     "is_day": 0,
-//     "condition": {
-//         "text": "Heavy snow",
-//         "icon": "//cdn.weatherapi.com/weather/64x64/night/338.png",
-//         "code": 1225
-//     },
-//     "wind_mph": 3.8,
-//     "wind_kph": 6.1,
-//     "wind_degree": 326,
-//     "wind_dir": "NNW",
-//     "pressure_mb": 1028,
-//     "pressure_in": 30.35,
-//     "precip_mm": 2,
-//     "precip_in": 0.08,
-//     "humidity": 75,
-//     "cloud": 100,
-//     "feelslike_c": -9.2,
-//     "feelslike_f": 15.4,
-//     "vis_km": 2,
-//     "vis_miles": 1,
-//     "uv": 1,
-//     "gust_mph": 4.5,
-//     "gust_kph": 7.2
-// }
+function setLocation(results) {
+    const locNameSpan = document.getElementById("locationName")
+    locNameSpan.textContent = results.location.name;
+
+    const locCountrySpan = document.getElementById("locationCountry")
+    locCountrySpan.textContent = results.location.country;
+
+    const locRegionSpan = document.getElementById("locationRegion")
+    locRegionSpan.textContent = results.location.region;
+
+    const locCoordSpan = document.getElementById("locationCoord")
+    locCoordSpan.textContent = `Lat: ${results.location.lat} Lon: ${results.location.lon}`;
+}
+
+function setResults(results) {
+    const tempCSpan = document.getElementById("tempCResult")
+    tempCSpan.textContent = results.current.temp_c;
+    
+    const tempFSpan = document.getElementById("tempFResult")
+    tempFSpan.textContent = results.current.temp_f;
+        
+    const humiditySpan = document.getElementById("humidityResult")
+    humiditySpan.textContent = results.current.humidity;
+    
+    const windMPHSpan = document.getElementById("windMPHResult")
+    windMPHSpan.textContent = results.current.wind_mph;
+    
+    const windDegreeSpan = document.getElementById("windDegreeResult")
+    windDegreeSpan.textContent = results.current.wind_degree;
+    
+    const windDirSpan = document.getElementById("windDirectionResult")
+    windDirSpan.textContent = results.current.wind_dir;
+    
+    const feelsLikeCSpan = document.getElementById("feelsLikeCResult")
+    feelsLikeCSpan.textContent = results.current.feelslike_c;
+    
+    const feelsLikeFSpan = document.getElementById("feelsLikeFResult")
+    feelsLikeFSpan.textContent = results.current.feelslike_f;
+    
+    const pressumeMBSpan = document.getElementById("pressureMBResult")
+    pressumeMBSpan.textContent = results.current.pressure_mb;
+    
+    const precipParaSpan = document.getElementById("precipitationMMResult")
+    precipParaSpan.textContent = results.current.precip_mm;
+    
+    const uvSpan = document.getElementById("uvResult")
+    uvSpan.textContent = results.current.uv;
+}
+
+function hideLoadingStatus(status) {
+    // status is true or false
+    const loadingPara = document.getElementById("loading")
+    loadingPara.hidden = status;
+}
 
 async function getWeather() {
+    hideLoadingStatus(false)
     const baseURL = "http://api.weatherapi.com/v1"
     const {location, airQuality, selected } = inputtedData()
 
     const response = await fetch(`${baseURL}/${selected}.json?key=${key}&q=${location}&q=${airQuality}`);
     if (response.ok) {
         const body = await response.json()
-        console.log('bod', body.current);
+        console.log('bod', body);
         setResults(body)
-        console.log('bod', body.forecast);
-        console.log('bod', body.location);
+        setLocation(body)
+        hideFailure()
+        hideLoadingStatus(true)
     } else {
         const body = await response.json()
         showFailure(body.error.message)
+        hideLoadingStatus(true)
     }
 }
 
