c5686b77bc92a5b159dc1890811119fc04158e7d
KarlH3644 <43153810+karl3644@users.noreply.github.com>
2023-05-22 11:54:33 +0100

    add html, css and objects

---
+++ Intermediate - Tic Tac Toe/index.js
@@ -0,0 +1,13 @@
+// one gameBoard. use a module
+const gameBoard = {
+  gameBoard: ["X", "X", "X", "X", "X", "O", "O", "O", "O"],
+};
+
+// one. use a module
+const displayController = {
+  showSquare: function () {},
+};
+
+// players - multiple. use a factory
+const playerOne = {};
+const playerTwo = {};
