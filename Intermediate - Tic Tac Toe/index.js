288273e4807a6db914d5ca5eae165fa7d6031936
karl3644 <43153810+karl3644@users.noreply.github.com>
2023-05-28 13:52:55 +0100

    add logic for player turns

--- Intermediate - Tic Tac Toe/index.js
+++ Intermediate - Tic Tac Toe/index.js
@@ -1,26 +1,90 @@
 // module
 // use to control init game, turns, testing for winner
-const gameBoard = () => {
-  const gameBoard = ["X", "X", "X", "X", "X", "O", "O", "O", "O"];
+const gameBoard = (() => {
+  const gameBoard = ["x", "x", "y", "", "y", "", "", "", ""];
+  let turn;
+
+  // getUserName()
+
+  const decideInitialSymbol = () => {
+    // if turn undefined the game has just started
+    if (turn === undefined) {
+      Math.round(Math.random()) === 0 ? (turn = "O") : (turn = "X");
+    }
+    return { turn };
+  };
+
+  const decidePlayerSymbol = () => {
+    let playerOneSymbol = "X";
+    let playerTwoSymbol = "O";
+    if (Math.round(Math.random()) === 0) {
+      playerOneSymbol = "O";
+      playerTwoSymbol = "X";
+    }
+    return { playerOneSymbol, playerTwoSymbol };
+  };
+
+  const firstPlayer = () => {
+    if (decideInitialSymbol().turn === decidePlayerSymbol().playerOneSymbol) {
+      // playerOne = 1;
+      // playerTwo = 2;
+    } else {
+      // playerOne = 2;
+      // playerTwo = 1;
+    }
+  };
+
+  const swapSymbol = () => {
+    turn === "X" ? (turn = "O") : turn === "O" ? (turn = "X") : null;
+    return { turn };
+  };
+
   // const initGame = () => {}
-  return { gameBoard };
-};
+  // getUserInput(name)
+  // decideInitialSymbol
+  // decide player turn
+
+  return {
+    gameBoard,
+    decidePlayerSymbol,
+    decideInitialSymbol,
+    swapSymbol,
+    firstPlayer,
+  };
+})();
 // module
 // only use for taking user input and updating the UI
-const displayController = () => {
+const displayController = (() => {
+  const allCells = Array.from(document.querySelectorAll(".cell"));
+
   const clickHandlerBoard = () => {
-    console.log("test");
+    allCells.forEach((cell) => {
+      cell.addEventListener("click", () => {
+        console.log(cell.id);
+      });
+    });
   };
-  // updateScreen
-  return { clickHandlerBoard };
-};
+
+  const updateScreen = () => {
+    if (allCells.length) {
+      for (let i = 0; i < gameBoard.gameBoard.length; i++) {
+        allCells[i].textContent = gameBoard.gameBoard[i];
+      }
+    }
+  };
+
+  return { clickHandlerBoard, updateScreen };
+})();
 
 // players - multiple. use a factory
-const player = (name, number) => {
-  return { name, number };
+const player = (name, number, symbol) => {
+  return { name, number, symbol };
 };
-const playerOne = player("john", 1);
-const playerTwo = player("smith", 2);
-console.log(playerOne, playerTwo);
+const playerOne = player("john");
+const playerTwo = player("smith");
 
-displayController().clickHandlerBoard();
+// displayController.updateScreen();
+// displayController.clickHandlerBoard();
+// gameBoard.decideInitialSymbol();
+// gameBoard.swapSymbol();
+gameBoard.firstPlayer();
